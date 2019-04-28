288fed90bdf3e8fa236d734f93ecdc63a44c6af2
geweiHome@163.com <geweiHome@163.com>
2019-04-28 20:09:13 +0800

    测试

---
+++ dao/src/main/java/com/gewei/demo/dao/DemoDao.java
@@ -0,0 +1,10 @@
+package com.gewei.demo.dao;
+
+import lombok.Data;
+
+@Data
+public class DemoDao {
+    private String demoName;
+    private String demoNo;
+    private String demoDesc;
+}
---
+++ service/src/main/java/com/gewei/demo/service/DemoService.java
@@ -0,0 +1,8 @@
+package com.gewei.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DemoService {
+     //DemoDao getDemoDao();
+}
